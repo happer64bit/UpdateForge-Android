7bd0a6fa6abb87d27598f1b009991ef968c76396
happer64bit <happer64bit@gmail.com>
2023-12-02 16:11:41 +0630

    Added AppBar

---
+++ components/AppBar.tsx
@@ -0,0 +1,33 @@
+import { Text, TouchableOpacity, View } from "react-native";
+import Icon from 'react-native-vector-icons/Feather'
+
+export default function AppBar({ title, isBackable, navigation } : { title: string, isBackable: true | boolean, navigation: any }) {
+    return (
+        <View style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            flexDirection: "row",
+            paddingHorizontal: 20,
+            paddingVertical: 15
+        }}>
+            <View style={{
+                display: "flex",
+                alignItems: "center",
+                gap: 10,
+                flexDirection: "row"
+            }}>
+                {isBackable && (
+                    <TouchableOpacity onPress={() => navigation.pop() }>
+                        <Icon name="arrow-left" color={"#111"} size={20}/>
+                    </TouchableOpacity>
+                )}
+                <Text style={{
+                    fontSize: 20,
+                    fontWeight: "bold",
+                    color: "#111"
+                }}>{title}</Text>
+            </View>
+        </View>
+    )
+}
